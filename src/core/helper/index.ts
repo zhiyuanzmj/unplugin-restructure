d58879cadd3359dccf97c23f89f09c006346618f
zhiyuanzmj <260480378@qq.com>
2025-03-06 15:33:16 +0800

    chore: remove shim.d.ts

--- src/core/helper/index.ts
+++ src/core/helper/index.ts
@@ -1,4 +1,5 @@
 export const helperPrefix: '/unplugin-restructure' = `/unplugin-restructure`
 
 export const withDefaultsHelperId: '/unplugin-restructure/with-defaults.js' = `${helperPrefix}/with-defaults.js`
+// @ts-ignore
 export { default as withDefaultsHelperCode } from './with-defaults.js?raw'
